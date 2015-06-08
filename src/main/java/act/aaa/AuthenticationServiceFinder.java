779ff330afdacfe2badd99440956a2474092adb0
Gelin Luo <greenlaw110@gmail.com>
2015-06-08 12:10:54 +1000

    initial commit

--- src/main/java/act/aaa/AuthenticationServiceFinder.java
+++ src/main/java/act/aaa/AuthenticationServiceFinder.java
@@ -5,7 +5,7 @@ import act.app.App;
 import act.app.AppByteCodeScanner;
 import act.util.SubTypeFinder;
 import org.osgl._;
-import org.osgl.aaa.AAA;
+import org.osgl.aaa.AuthenticationService;
 import org.osgl.aaa.AuthorizationService;
 import org.osgl.exception.NotAppliedException;
 
@@ -13,23 +13,23 @@ import java.lang.reflect.Modifier;
 import java.util.Map;
 import java.util.Set;
 
-public class AuthorizationServiceFinder extends SubTypeFinder {
-    public AuthorizationServiceFinder() {
-        super(AuthorizationService.class, new _.F2<App, String, Map<Class<? extends AppByteCodeScanner>, Set<String>>>() {
+public class AuthenticationServiceFinder extends SubTypeFinder {
+    public AuthenticationServiceFinder() {
+        super(AuthenticationService.class, new _.F2<App, String, Map<Class<? extends AppByteCodeScanner>, Set<String>>>() {
             public Map<Class<? extends AppByteCodeScanner>, Set<String>> apply(final App app, final String className) throws NotAppliedException, _.Break {
-                final Class<? extends AuthorizationService> c = _.classForName(className, app.classLoader());
+                final Class<? extends AuthenticationService> c = _.classForName(className, app.classLoader());
                 if (Modifier.isAbstract(c.getModifiers())) {
                     return null;
                 }
                 app.jobManager().afterAppStart(new Runnable() {
                     @Override
                     public void run() {
-                        AuthorizationService service = app.newInstance(c);
+                        AuthenticationService service = app.newInstance(c);
                         AAAPlugin plugin = Act.sessionManager().findListener(AAAPlugin.class);
                         if (null == plugin) {
                             logger.error("AAAPlugin not found");
                         } else {
-                            plugin.authorizationService = service;
+                            plugin.authenticationService = service;
                         }
                     }
                 });
