015cd33a6b15144f452cf5f1c81c52162e734f04
Gelin Luo <greenlaw110@gmail.com>
2016-01-07 06:39:47 +1100

    implement a default permissions() method in DynamicPermissionCheckHelperBase class

--- src/main/java/act/aaa/DynamicPermissionCheckHelperBase.java
+++ src/main/java/act/aaa/DynamicPermissionCheckHelperBase.java
@@ -1,9 +1,17 @@
 package act.aaa;
 
 import org.osgl.aaa.DynamicPermissionCheckHelper;
+import org.osgl.aaa.Permission;
+import org.osgl.util.C;
+
+import java.util.List;
 
 public abstract class DynamicPermissionCheckHelperBase<T> implements DynamicPermissionCheckHelper<T>{
 
     protected abstract Class<T> getTargetClass();
 
+    @Override
+    public List<? extends Permission> permissions() {
+        return C.list();
+    }
 }
