356e0a2f293216fe7b2a15afd1512e2054fb52b8
Gelin Luo <greenlaw110@gmail.com>
2016-01-07 06:56:01 +1100

    add permissionNames() method to DyanmicPermissionCheckHelperBase class

--- src/main/java/act/aaa/DynamicPermissionCheckHelperBase.java
+++ src/main/java/act/aaa/DynamicPermissionCheckHelperBase.java
@@ -1,7 +1,7 @@
 package act.aaa;
 
-import org.osgl.aaa.DynamicPermissionCheckHelper;
-import org.osgl.aaa.Permission;
+import org.osgl.$;
+import org.osgl.aaa.*;
 import org.osgl.util.C;
 
 import java.util.List;
@@ -10,8 +10,32 @@ public abstract class DynamicPermissionCheckHelperBase<T> implements DynamicPerm
 
     protected abstract Class<T> getTargetClass();
 
+    /**
+     *
+     * @return
+     */
     @Override
     public List<? extends Permission> permissions() {
+        List<String> names = permissionNames();
+        if (names.isEmpty()) {
+            return C.list();
+        }
+        AAAContext aaa = AAA.context();
+        AAAPersistentService ps = aaa.getPersistentService();
+        List<Permission> perms = C.newList();
+        for (String name: names) {
+            Permission p = ps.findByName(name, Permission.class);
+            perms.add($.notNull(p));
+        }
+        return perms;
+    }
+
+    /**
+     * Returns a list of permission name strings. By default
+     * this method will return an empty list.
+     * @return permission names in a string list
+     */
+    protected List<String> permissionNames() {
         return C.list();
     }
 }
