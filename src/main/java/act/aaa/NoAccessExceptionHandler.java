c965917ae3063d1323be7af6158e607768da7c16
Gelin Luo <greenlaw110@gmail.com>
2017-02-01 08:09:20 +1100

    minor code update in NoAccessExceptionHandler

--- src/main/java/act/aaa/NoAccessExceptionHandler.java
+++ src/main/java/act/aaa/NoAccessExceptionHandler.java
@@ -1,23 +1,17 @@
 package act.aaa;
 
-import act.Act;
-import act.ActComponent;
 import act.app.ActionContext;
 import act.handler.builtin.controller.ActionHandlerInvoker;
 import act.handler.builtin.controller.ExceptionInterceptor;
 import act.view.ActForbidden;
 import org.osgl.aaa.NoAccessException;
-import org.osgl.mvc.result.Forbidden;
 import org.osgl.mvc.result.Result;
 
 public class NoAccessExceptionHandler extends ExceptionInterceptor {
 
-    private final boolean isDev;
-
     @SuppressWarnings("unchecked")
     public NoAccessExceptionHandler() {
         super(0, NoAccessException.class);
-        isDev = Act.isDev();
     }
 
     @Override
@@ -37,6 +31,6 @@ public class NoAccessExceptionHandler extends ExceptionInterceptor {
 
     @Override
     protected Result internalHandle(Exception e, ActionContext actionContext) {
-        return isDev ? new ActForbidden(e) : Forbidden.INSTANCE;
+        return ActForbidden.create(e);
     }
 }
